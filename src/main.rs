f4bbcc2ae50f79de525cdb221979922203eafd98
takahirokudo0 <dlf.lycoris@gmail.com>
2021-05-11 07:56:28 +0900

    variable test

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
 
 fn main() {
     hello_world();
+    variable();
 }
 
 // snake case
 fn hello_world() {
     println!("Hello, world!");
 }
+
+fn variable() {
+    let x = 5;
+    let s = "abc";
+    println!("{1}, {0}", s, x);
+}
+
